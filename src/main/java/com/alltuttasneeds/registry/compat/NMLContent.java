2c9f888400635bbb15663e8576184bd5d56088e7
Tutitta <111702644+SoyTutta@users.noreply.github.com>
2025-07-31 17:31:28 -0300

    soy tontisima

--- src/main/java/com/alltuttasneeds/registry/compat/NMLContent.java
+++ src/main/java/com/alltuttasneeds/registry/compat/NMLContent.java
@@ -24,7 +24,7 @@ public class NMLContent {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, "nomansland");
     public static final DeferredRegister<Item>  ITEMS  = DeferredRegister.create(Registries.ITEM,  "nomansland");
 
-    public static final Map<String, Supplier<? extends Block>> DOORS      = new HashMap<>();
+    public static final Map<String, Supplier<? extends Block>> DOORS = new HashMap<>();
     public static final Map<String, Supplier<Item>>  DOOR_ITEMS = new HashMap<>();
 
     private static class SafeAccess {
@@ -37,38 +37,55 @@ public class NMLContent {
         static final Supplier<BlockSetType> WILLOW_SET = () -> ((DoorBlock) WILLOW_DOOR.get()).type();
         static final Supplier<BlockSetType> WALNUT_SET = () -> ((DoorBlock) WALNUT_DOOR.get()).type();
         static final Supplier<BlockSetType> MAPLE_SET  = () -> ((DoorBlock) MAPLE_DOOR.get()).type();
+
+        static final Supplier<Block> PINE_BOOKSHELF = NMLBlocks.PINE.bookshelf();
+        static final Supplier<Block> WILLOW_BOOKSHELF = NMLBlocks.WILLOW.bookshelf();
+        static final Supplier<Block> WALNUT_BOOKSHELF = NMLBlocks.WALNUT.bookshelf();
+        static final Supplier<Block> MAPLE_BOOKSHELF = NMLBlocks.MAPLE.bookshelf();
+
+        static final Supplier<Block> SPRUCE_BOOKSHELF = NMLBlocks.SPRUCE_BOOKSHELF;
+        static final Supplier<Block> BIRCH_BOOKSHELF = NMLBlocks.BIRCH_BOOKSHELF;
+        static final Supplier<Block> JUNGLE_BOOKSHELF = NMLBlocks.JUNGLE_BOOKSHELF;
+        static final Supplier<Block> ACACIA_BOOKSHELF = NMLBlocks.ACACIA_BOOKSHELF;
+        static final Supplier<Block> DARK_OAK_BOOKSHELF = NMLBlocks.DARK_OAK_BOOKSHELF;
+        static final Supplier<Block> MANGROVE_BOOKSHELF = NMLBlocks.MANGROVE_BOOKSHELF;
+        static final Supplier<Block> CHERRY_BOOKSHELF = NMLBlocks.CHERRY_BOOKSHELF;
+        static final Supplier<Block> BAMBOO_BOOKSHELF = NMLBlocks.BAMBOO_BOOKSHELF;
+        static final Supplier<Block> CRIMSON_BOOKSHELF = NMLBlocks.CRIMSON_BOOKSHELF;
+        static final Supplier<Block> WARPED_BOOKSHELF = NMLBlocks.WARPED_BOOKSHELF;
     }
 
     private record WoodConfig(Supplier<BlockSetType> type, String woodName, Supplier<? extends Block> baseDoor, boolean discrete, boolean normal, boolean indiscrete, boolean transit, boolean pet) {}
-
-    private static final List<WoodConfig> WOOD_CONFIGS = List.of(
-            new WoodConfig( SafeAccess.PINE_SET,   "pine",   SafeAccess.PINE_DOOR,  true,  false, true, true, true),
-            new WoodConfig( SafeAccess.WILLOW_SET, "willow", SafeAccess.WILLOW_DOOR,true,  false, true, true, true),
-            new WoodConfig( SafeAccess.WALNUT_SET, "walnut", SafeAccess.WALNUT_DOOR,true,  false, true, true, true),
-            new WoodConfig( SafeAccess.MAPLE_SET,  "maple",  SafeAccess.MAPLE_DOOR, false, true,  true, true, true)
-    );
-
     private record SecretDoorConfig(String woodName, Supplier<Block> bookshelf, Supplier<BlockSetType> setType) {}
 
-    private static final List<SecretDoorConfig> SECRET_DOOR_CONFIGS = List.of(
-            new SecretDoorConfig("pine",   () -> NMLBlocks.PINE.bookshelf().get(),   SafeAccess.PINE_SET),
-            new SecretDoorConfig("willow", () -> NMLBlocks.WILLOW.bookshelf().get(), SafeAccess.WILLOW_SET),
-            new SecretDoorConfig("walnut", () -> NMLBlocks.WALNUT.bookshelf().get(), SafeAccess.WALNUT_SET),
-            new SecretDoorConfig("maple",  () -> NMLBlocks.MAPLE.bookshelf().get(),  SafeAccess.MAPLE_SET),
-
-            new SecretDoorConfig("spruce",   () -> NMLBlocks.SPRUCE_BOOKSHELF.get(),   () -> BlockSetType.SPRUCE),
-            new SecretDoorConfig("birch",    () -> NMLBlocks.BIRCH_BOOKSHELF.get(),    () -> BlockSetType.BIRCH),
-            new SecretDoorConfig("jungle",   () -> NMLBlocks.JUNGLE_BOOKSHELF.get(),   () -> BlockSetType.JUNGLE),
-            new SecretDoorConfig("acacia",   () -> NMLBlocks.ACACIA_BOOKSHELF.get(),   () -> BlockSetType.ACACIA),
-            new SecretDoorConfig("dark_oak", () -> NMLBlocks.DARK_OAK_BOOKSHELF.get(), () -> BlockSetType.DARK_OAK),
-            new SecretDoorConfig("mangrove", () -> NMLBlocks.MANGROVE_BOOKSHELF.get(), () -> BlockSetType.MANGROVE),
-            new SecretDoorConfig("cherry",   () -> NMLBlocks.CHERRY_BOOKSHELF.get(),   () -> BlockSetType.CHERRY),
-            new SecretDoorConfig("bamboo",   () -> NMLBlocks.BAMBOO_BOOKSHELF.get(),   () -> BlockSetType.BAMBOO),
-            new SecretDoorConfig("crimson",  () -> NMLBlocks.CRIMSON_BOOKSHELF.get(),  () -> BlockSetType.CRIMSON),
-            new SecretDoorConfig("warped",   () -> NMLBlocks.WARPED_BOOKSHELF.get(),   () -> BlockSetType.WARPED)
-    );
+    private static List<WoodConfig> WOOD_CONFIGS;
+    private static List<SecretDoorConfig> SECRET_DOOR_CONFIGS;
 
     static {
+        WOOD_CONFIGS = List.of(
+                new WoodConfig( SafeAccess.PINE_SET,   "pine",   SafeAccess.PINE_DOOR,  true,  false, true, true, true),
+                new WoodConfig( SafeAccess.WILLOW_SET, "willow", SafeAccess.WILLOW_DOOR,true,  false, true, true, true),
+                new WoodConfig( SafeAccess.WALNUT_SET, "walnut", SafeAccess.WALNUT_DOOR,true,  false, true, true, true),
+                new WoodConfig( SafeAccess.MAPLE_SET,  "maple",  SafeAccess.MAPLE_DOOR, false, true,  true, true, true)
+        );
+
+        SECRET_DOOR_CONFIGS = List.of(
+                new SecretDoorConfig("pine",   SafeAccess.PINE_BOOKSHELF,   SafeAccess.PINE_SET),
+                new SecretDoorConfig("willow", SafeAccess.WILLOW_BOOKSHELF, SafeAccess.WILLOW_SET),
+                new SecretDoorConfig("walnut", SafeAccess.WALNUT_BOOKSHELF, SafeAccess.WALNUT_SET),
+                new SecretDoorConfig("maple",  SafeAccess.MAPLE_BOOKSHELF,  SafeAccess.MAPLE_SET),
+                new SecretDoorConfig("spruce",   SafeAccess.SPRUCE_BOOKSHELF,   () -> BlockSetType.SPRUCE),
+                new SecretDoorConfig("birch",    SafeAccess.BIRCH_BOOKSHELF,    () -> BlockSetType.BIRCH),
+                new SecretDoorConfig("jungle",   SafeAccess.JUNGLE_BOOKSHELF,   () -> BlockSetType.JUNGLE),
+                new SecretDoorConfig("acacia",   SafeAccess.ACACIA_BOOKSHELF,   () -> BlockSetType.ACACIA),
+                new SecretDoorConfig("dark_oak", SafeAccess.DARK_OAK_BOOKSHELF, () -> BlockSetType.DARK_OAK),
+                new SecretDoorConfig("mangrove", SafeAccess.MANGROVE_BOOKSHELF, () -> BlockSetType.MANGROVE),
+                new SecretDoorConfig("cherry",   SafeAccess.CHERRY_BOOKSHELF,   () -> BlockSetType.CHERRY),
+                new SecretDoorConfig("bamboo",   SafeAccess.BAMBOO_BOOKSHELF,   () -> BlockSetType.BAMBOO),
+                new SecretDoorConfig("crimson",  SafeAccess.CRIMSON_BOOKSHELF,  () -> BlockSetType.CRIMSON),
+                new SecretDoorConfig("warped",   SafeAccess.WARPED_BOOKSHELF,   () -> BlockSetType.WARPED)
+        );
+
         for (NMLContent.WoodConfig cfg : WOOD_CONFIGS) {
             if (cfg.discrete())   register(cfg.woodName(), "discrete_door",   cfg.type(), cfg.baseDoor(), DoorBlock::new);
             if (cfg.normal())     register(cfg.woodName(), "normal_door",     cfg.type(), cfg.baseDoor(), DoorBlock::new);
--- src/main/java/com/alltuttasneeds/registry/events/ClientModBusEvents.java
+++ src/main/java/com/alltuttasneeds/registry/events/ClientModBusEvents.java
@@ -12,27 +12,11 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
 @EventBusSubscriber(modid = AllTuttasNeeds.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientModBusEvents {
-
-    private record CompatModule(Supplier<Boolean> isLoaded, Map<String, Supplier<? extends Block>> doorMap) {}
-
-    private static final List<CompatModule> STANDARD_COMPAT_MODULES = List.of(
-            new CompatModule(() -> true, TDContent.DOORS),
-            new CompatModule(Mods.NOMANSLAND::isLoaded, NMLContent.DOORS),
-            new CompatModule(Mods.BLOCKBOX::isLoaded, BBContent.DOORS),
-            new CompatModule(Mods.NEWWORLD::isLoaded, NWContent.DOORS),
-            new CompatModule(Mods.MYNETHERSDELIGHT::isLoaded, MNDContent.DOORS),
-            new CompatModule(Mods.ENDERSCAPE::isLoaded, ESContent.DOORS),
-            new CompatModule(Mods.ARTS_AND_CRAFTS::isLoaded, ACContent.DOORS),
-            new CompatModule(Mods.NATURES_SPIRIT::isLoaded, NSContent.DOORS),
-            new CompatModule(Mods.CREATE::isLoaded, CreateContent.DOORS),
-            new CompatModule(Mods.MALUM::isLoaded, MalumContent.DOORS)
-    );
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
@@ -40,15 +24,37 @@ public class ClientModBusEvents {
             RenderType cutoutLayer = RenderType.cutout();
             RenderType translucentLayer = RenderType.translucent();
 
-            for (CompatModule module : STANDARD_COMPAT_MODULES) {
-                if (module.isLoaded().get() && module.doorMap() != null) {
-                    module.doorMap().values().forEach(blockSupplier ->
-                            ItemBlockRenderTypes.setRenderLayer(blockSupplier.get(), cutoutLayer)
-                    );
-                }
+            setRenderLayers(TDContent.DOORS, cutoutLayer);
+
+            if (Mods.NOMANSLAND.isLoaded()) {
+                setRenderLayers(NMLContent.DOORS, cutoutLayer);
+            }
+            if (Mods.BLOCKBOX.isLoaded()) {
+                setRenderLayers(BBContent.DOORS, cutoutLayer);
+            }
+            if (Mods.NEWWORLD.isLoaded()) {
+                setRenderLayers(NWContent.DOORS, cutoutLayer);
+            }
+            if (Mods.MYNETHERSDELIGHT.isLoaded()) {
+                setRenderLayers(MNDContent.DOORS, cutoutLayer);
+            }
+            if (Mods.ENDERSCAPE.isLoaded()) {
+                setRenderLayers(ESContent.DOORS, cutoutLayer);
+            }
+            if (Mods.ARTS_AND_CRAFTS.isLoaded()) {
+                setRenderLayers(ACContent.DOORS, cutoutLayer);
+            }
+            if (Mods.NATURES_SPIRIT.isLoaded()) {
+                setRenderLayers(NSContent.DOORS, cutoutLayer);
+            }
+            if (Mods.CREATE.isLoaded()) {
+                setRenderLayers(CreateContent.DOORS, cutoutLayer);
+            }
+            if (Mods.MALUM.isLoaded()) {
+                setRenderLayers(MalumContent.DOORS, cutoutLayer);
             }
 
-            if (Mods.BIOMESOPLENTY.isLoaded() && BoPContent.DOORS != null) {
+            if (Mods.BIOMESOPLENTY.isLoaded()) {
                 BoPContent.DOORS.forEach((name, blockSupplier) -> {
                     if (name.startsWith("magic_")) {
                         ItemBlockRenderTypes.setRenderLayer(blockSupplier.get(), translucentLayer);
@@ -58,5 +64,11 @@ public class ClientModBusEvents {
                 });
             }
         });
+    }
+
+    private static void setRenderLayers(Map<String, Supplier<? extends Block>> doorMap, RenderType renderType) {
+        if (doorMap != null) {
+            doorMap.values().forEach(blockSupplier -> ItemBlockRenderTypes.setRenderLayer(blockSupplier.get(), renderType));
+        }
     }
 }
